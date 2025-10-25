80befb035e95502a2005b5fee022a0349a839ac3
EGGorkaa <egor_novikov20004@mail.ru>
2025-10-25 14:26:32 +0300

    new update for main.cpp

--- main.cpp
+++ main.cpp
@@ -2,4 +2,12 @@ int main(){
   using u_t = unsigned;
   u_t a = 0;
   std::cin >> a;
+  if(std::cin.eof()){
+  std::cout << 0;
+  std::cout << "\n";
+  }
+  esle if (std::cin.fail()){
+  std::cerr << "input err\n";
+  return 1;
+  }
 }

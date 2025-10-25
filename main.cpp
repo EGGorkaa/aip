3fc2db3c08e3d832ccbb1cd7a60e9e2160ba0df1
EGGorkaa <egor_novikov20004@mail.ru>
2025-10-25 15:04:37 +0300

    yeah! its working!(mb, idk)

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 
+bool isPyth(unsigned a, unsigned b, unsigned c);
+
 int main(){
   using u_t = unsigned;
-  u_t a = 0;
+  u_t a = 0, b = 0, c = 0;
   size_t count = 0;
-  //std::cin >> a;
+  std::cin >> b >> c;
   while (std::cin>>a){
-    count++;
+    count+=isPyth(a, b, c) ? 1 : 0;
+    c=b;
+    b=a;
   }
   if(std::cin.eof()){
     std::cout << count;
@@ -16,4 +20,11 @@ int main(){
     std::cerr << "input err\n";
     return 1;
   }
+}
+
+bool isPyth (unsigned a, unsigned b, unsigned c){
+  bool p = a*a==(b*b+c*c);
+  p = p||b*b==(a*a+c*c);
+  p = p||c*c==(a*a+b*b);
+  return p;
 }
